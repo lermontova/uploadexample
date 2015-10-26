f96a652f614ce242910abf5864d655d11b7a1425
Valery <v.tsybulko@favbet.com>
2015-10-26 17:50:32 +0200

    seconf and working

--- uploadexample.js
+++ uploadexample.js
@@ -1,23 +1,33 @@
+Uploads = new FS.Collection('uploads',{
+stores:[new FS.Store.FileSystem('uploads',{path:'~/projectUploads'})]
+});
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+// counter starts at 0
+Session.setDefault("counter", 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+Template.hello.helpers({
+counter: function () {
+  return Session.get("counter");
+},
+uploads:function(){
+  return Uploads.find();
+}
+});
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+Template.hello.events({
+'change .fileInput':function(event,tmpl){
+  FS.Utility.eachFile(event,function(file){
+    var fileObj = new FS.File(file);
+    Uploads.insert(fileObj),function(err){
+      console.log(err);
     }
-  });
+  })
+}
+});
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+Meteor.startup(function () {
+// code to run on server at startup
+});
 }
